59fc1f6a891e7cc29b2972819ac72a371b12c4d5
Lucas <LukkyzYT@gmail.com>
2020-05-02 03:32:33 +0200

    added pom.xml

---
+++ src/main/java/de/lukkyz/lobsterbot/Secrets.java
@@ -0,0 +1,8 @@
+package de.lukkyz.lobsterbot;
+
+public class Secrets {
+
+    /* Bot Token */
+    public static final String BOT_TOKEN = "NjMwMTIzNDE2OTM4MzQ4NTY0.Xoxx6g.lx1JR1cg9rqOHeMANHpZ7U8b2xA";
+
+}
